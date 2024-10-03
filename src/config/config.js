a67597bd5378a49ccb7ccdf8db8175f3335b6cee
nhatnguyen150100 <nhatnguyen150100>
2024-10-03 17:43:53 +0700

    fix: config db

--- src/config/config.js
+++ src/config/config.js
@@ -1,43 +1,41 @@
-'use-strict'
+"use-strict";
 const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT,
+  development: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_DATABASE || "db_nihongo",
+    host: process.env.DB_HOST || "localhost",
+    dialect: process.env.DB_DIALECT || "mysql",
     dialectOptions:
-    process.env.DB_SSL === 'true' ?
-      {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      } : {}
-    ,
-    "logging":false,
-    "query": {
-      "raw": true
+      process.env.DB_SSL === "true"
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+    logging: false,
+    query: {
+      raw: true,
     },
-    "timezone":"+07:00"
+    timezone: "+07:00",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
-  }
-}
-
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+};
